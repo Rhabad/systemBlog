f0007b4e575055130931cc0b1e06a11ff867e717
Nicolas <nicolaspizarro.450@gmail.com>
2024-03-11 10:42:40 -0300

    no se porque coloque ese @Bean

--- src/main/java/com/sistemblog/config/SecurityConfig.java
+++ src/main/java/com/sistemblog/config/SecurityConfig.java
@@ -1,19 +1,18 @@
 package com.sistemblog.config;
 
+import com.sistemblog.services.security.CustomUserDetailsService;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,25 +20,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
+    @Autowired
+    private CustomUserDetailsService customService;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails nico = User.builder()
-                .username("nico")
-                .password(passwordEncoder().encode("nico"))
-                .roles("USER")
-                .build();
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
 
-        return new InMemoryUserDetailsManager(nico, admin);
+
+    public void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(customService).passwordEncoder(passwordEncoder());
     }
 
     @Bean
