bf2ae0583fac985d2adbbf977f2187dac87596e0
Nicolas <nicolaspizarro.450@gmail.com>
2024-03-11 10:41:52 -0300

    dto login

---
+++ src/main/java/com/sistemblog/models/dto/LoginDto.java
@@ -0,0 +1,11 @@
+package com.sistemblog.models.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class LoginDto {
+    private String usernameOrEmail;
+    private String password;
+}
